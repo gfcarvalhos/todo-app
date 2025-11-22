93ae3c2e329b077c4303e219a07d9cbfe5f545c8
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-11-22 07:46:46 -0300

    feat: implementa UI do frontend com estado local (sem API)

--- src/todo/frontend/core.cljs
+++ src/todo/frontend/core.cljs
@@ -1,21 +1,50 @@
 (ns todo.frontend.core
 (:require [reagent.core :as r]
-[reagent.dom.client :as rdom]))
+          [reagent.dom.client :as rdom]
+          [clojure.string :as str]))
 
-(defonce app-state (r/atom 0))
+(defonce app-state (r/atom {:next-id 1 :input-text "" :todos []}))
 
-(defn counter-app []
-[:div 
-[:h1 "Ententendo o 'r/atom'"]
-[:p "O valor atual do contador é: " @app-state]
-[:button 
-{:on-click #(swap! app-state inc)}
-"Clique para Incrementar"]
-[:button
-{:on-click #(reset! app-state 0)}
-"Resetar"]])
+(defn adicionar-todo-local []
+  (swap! app-state
+    (fn [estado-atual]
+      (let [novo-titulo (:input-text estado-atual)
+            novo-id (:next-id estado-atual)]
+          (if (str/blank? novo-titulo)
+            estado-atual
+            {:next-id (inc novo-id)
+              :input-text ""
+              :todos (conj (:todos estado-atual)
+                          {:id novo-id
+                            :title novo-titulo})}
+)))))
+
+(defn todo-form []
+  [:div.todo-input
+    [:input {:type "text" 
+              :placeholder "O que precisa ser feito?" 
+              :value (:input-text @app-state) 
+              :on-change #(swap! app-state assoc :input-text (-> % .-target .-value))}]
+    [:button 
+      {:on-click adicionar-todo-local}
+      "Adicionar (Local)"]])
+
+(defn todo-list []
+  [:ul.todo-list
+    (for [todo (:todos @app-state)]
+      ^{:key (:id todo)}
+      [:li.todo-item
+        (:title todo)])])
+
+(defn app []
+  [:div.todo-app
+    [:h1 "Todo App (Somente Frontend)"]
+    [:p "Isto é 100% local. Recarregue (F5) para ver os dados sumirem."]
+    [todo-form]
+    [todo-list]
+])
 
 (defn ^:export init []
 (println "Frontend App 'Contador' inicializando...")
 (let [root (rdom/create-root (js/document.getElementById "app"))]
-(.render root (r/as-element [counter-app]))))
+(.render root (r/as-element [app]))))
